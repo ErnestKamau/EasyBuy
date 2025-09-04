fc7236fe6e6b5a138157d957fcf1f57695683a34
Ernest Kamau <1.kamauernest@gmail.com>
2025-09-04 11:50:58 +0300

    added category model

---
+++ server/products/models.py
@@ -0,0 +1,22 @@
+from django.db import models
+from django.core.validators import MinValueValidator
+from decimal import Decimal
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+    is_active = models.BooleanField(default=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        return self.name
+    
+    class Meta:
+        db_table = 'categories'
+        verbose_name_plural = 'Categories'
+
+
+
+        
+        
+        
