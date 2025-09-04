dc464bb895389ce89408f3cf2c78a7aa6cea93b3
Ernest Kamau <1.kamauernest@gmail.com>
2025-09-04 11:23:44 +0300

    adds user model

--- server/accounts/models.py
+++ server/accounts/models.py
@@ -1,3 +1,28 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+class User(AbstractUser):
+    
+    GENDER_CHOICES = [
+        ('Male', 'Male'),
+        ('Female', 'Female'),
+    ]
+    
+    ROLE_CHOICES = [
+        ('admin', 'Admin'),
+        ('customer', 'Customer'),
+    ]
+    
+    phone_number = models.CharField(max_length=15, unique=True)
+    email = models.EmailField(unique=True)
+    gender = models.CharField(max_length=10, choices=GENDER_CHOICES),
+    role = models.CharField(max_length=15, choices=ROLE_CHOICES, default='customer')
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    
+    def __str__(self):
+        return f"{self.username} ({self.phone_number})"
+    
+    class Meta:
+        db_table = 'users'
+
