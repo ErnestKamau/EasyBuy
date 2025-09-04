66d6b7d41c1323685c4a92aa436103fe4e87359a
Ernest Kamau <1.kamauernest@gmail.com>
2025-09-04 15:38:33 +0300

    adds product/category views

--- server/easybuy_project/settings.py
+++ server/easybuy_project/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'rest_framework',
     'rest_framework_simplejwt',
     'corsheaders',
+    'django_filters',
     'accounts',
     'products',
     # 'orders', 
@@ -151,6 +152,9 @@ REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.IsAuthenticated',
     ],
+    'DEFAULT_FILTER_BACKENDS': [
+        'django_filters.rest_framework.DjangoFilterBackend',
+        ],
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 20,
 }
--- server/products/views.py
+++ server/products/views.py
@@ -1,3 +1,39 @@
-from django.shortcuts import render
+from rest_framework import generics, status
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+from django_filters.rest_framework import DjangoFilterBackend
+from django.db.models import F
+from .models import Category, Product
+from .serializers import CategorySerializer, ProductSerializer
 
-# Create your views here.
+class CategoryListCreateView(generics.ListCreateAPIView):
+    queryset = Category.objects.filter(is_active=True)
+    serializer_class = CategorySerializer
+    permission_classes = [IsAuthenticated]
+
+
+class ProductListCreateView(generics.ListCreateAPIView):
+    queryset = Product.objects.filter(is_active=True)
+    serializer_class = ProductSerializer
+    filter_backends = [DjangoFilterBackend]
+    filterset_fields = ['category', 'is_low_stock']
+    
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        # Add search functionality
+        search = self.request.query_params.get('search', None)
+        if search:
+            queryset = queryset.filter(name__icontains=search)
+        return queryset
+
+
+@api_view(['GET'])
+@permission_classes([IsAuthenticated])
+def low_stock_products(request):
+    """Get products with low stock levels"""
+    products = Product.objects.filter(in_stock__lte=F('minimum_stock'))
+    serializer = ProductSerializer(products, many=True)
+    return Response(serializer.data)
+
+
