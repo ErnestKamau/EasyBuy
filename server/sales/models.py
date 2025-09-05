b3307bbed9f6ae2639a38c8641cbb3bdd7c245c1
Ernest Kamau <1.kamauernest@gmail.com>
2025-09-05 22:15:05 +0300

    adds payments model

--- server/sales/models.py
+++ server/sales/models.py
@@ -127,3 +127,35 @@ class SaleItem(models.Model):
         db_table = 'sale_items'
         
         
+class Payments(models.Model):
+    PAYMENT_METHODS = [
+        ('cash', 'Cash'),
+        ('mpesa', 'M-Pesa'),
+        ('bank_transfer', 'Bank Transfer'),
+        ('card', 'Card'),
+    ]
+    
+    
+    sale = models.ForeignKey(
+        Sale,
+        on_delete=models.CASCADE,
+        related_name='payments'
+    )
+    method = models.CharField(max_length=15, choices=PAYMENT_METHODS)
+    amount = models.DecimalField(
+        max_digits=10,
+        decimal_places=2,
+        validators=[MinValueValidator(Decimal(0.00))]
+    )
+    reference = models.CharField(max_length=100, blank=True) # M-Pesa transaction_id
+    notes = models.TextField(blank=True)
+    paid_at = models.DateTimeField(auto_now_add=True)
+    
+    
+    def __str__(self):
+        return f'{self.method} - {self.amount}'
+    
+    class Meta:
+        db_table = 'payments'
+        
+        
