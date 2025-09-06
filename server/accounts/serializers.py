2f188e636f130687639867ebb9c0997d565a2cd9
Ernest Kamau <1.kamauernest@gmail.com>
2025-09-06 11:07:05 +0300

    creates serializer.py

---
+++ server/accounts/serializers.py
@@ -0,0 +1,30 @@
+from django.contrib.auth.password_validation import validate_password
+from rest_framework import serializers
+from .models import User
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User,
+        fields = ['id', 'username', 'email', 'phone_number', 'gender', 'role', 'is_active']
+
+
+class UserRegistrationSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True, validators=[validate_password])
+    password_confirm = serializers.CharField(write_only=True)
+    
+    class Meta:
+        model = User,
+        fields = ['username', 'email', 'phone_number', 'gender', 'role', 'password', 'password_confirm']
+        
+    def validate(self, attrs):
+        if attrs['password'] != attrs['password_confirm']:
+            raise serializers.ValidationError("passwords don't match")
+        return attrs
+    
+    def create(self, validated_data):
+        validated_data.pop('password_confirm')
+        user = User.objects.create_user(**validated_data)
+        return user
+    
+    
+    
