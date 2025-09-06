da46186eda961be53f58b9c8d73f2f1cac31255c
Ernest Kamau <1.kamauernest@gmail.com>
2025-09-06 10:48:43 +0300

    adds User Serializer

--- server/accounts/views.py
+++ server/accounts/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.contrib.auth.password_validation import validate_password
+from rest_framework import serializers
+from .models import User
 
-# Create your views here.
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User,
+        fields = ['id', 'username', 'email', 'phone_number', 'gender', 'role', 'is_active']
+
+
+
