5729cc5d7cad61b08a6e64190f9b379da4fb7ac9
Ernest Kamau <1.kamauernest@gmail.com>
2025-09-07 19:33:12 +0300

    register routes

---
+++ server/accounts/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path
+from rest_framework_simplejwt.views import TokenRefreshView
+from . import views
+
+app_name = 'accounts'
+
+urlpatterns = [
+    path('register/', views.register, name='register'),
+    path('login/', views.login, name='login'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('profile/', views.user_profile, name='profile')
+]
+
+
