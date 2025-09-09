a2153e4035b32d68db28ff7c8a72714338b24147
Ernest Kamau <1.kamauernest@gmail.com>
2025-09-09 11:29:37 +0300

    adds category views

--- server/products/views.py
+++ server/products/views.py
@@ -6,34 +6,21 @@ from django_filters.rest_framework import DjangoFilterBackend
 from django.db.models import F
 from .models import Category, Product
 from .serializers import CategorySerializer, ProductSerializer
+from .permissions import IsAdmin, IsReadOnly
 
-class CategoryListCreateView(generics.ListCreateAPIView):
+
+class CategoryListView(generics.ListAPIView):
     queryset = Category.objects.filter(is_active=True)
     serializer_class = CategorySerializer
-    permission_classes = [IsAuthenticated]
-
-
-class ProductListCreateView(generics.ListCreateAPIView):
-    queryset = Product.objects.filter(is_active=True)
-    serializer_class = ProductSerializer
-    filter_backends = [DjangoFilterBackend]
-    filterset_fields = ['category', 'is_low_stock']
+    permission_classes = [IsAuthenticated, IsReadOnly]
     
-    def get_queryset(self):
-        queryset = super().get_queryset()
-        # Add search functionality
-        search = self.request.query_params.get('search', None)
-        if search:
-            queryset = queryset.filter(name__icontains=search)
-        return queryset
 
+class CategoryCreateView(generics.CreateAPIView):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+    permission_classes = [IsAuthenticated, IsAdmin]
+    
+    
 
-@api_view(['GET'])
-@permission_classes([IsAuthenticated])
-def low_stock_products(request):
-    """Get products with low stock levels"""
-    products = Product.objects.filter(in_stock__lte=F('minimum_stock'))
-    serializer = ProductSerializer(products, many=True)
-    return Response(serializer.data)
 
 
